f598b4d544d51ba832e142ba5fe1614abc0657c3
kaonmir <thswpvm@cau.ac.kr>
2021-12-27 18:22:35 +0900

    Switch port form 80 to 3000. We can't use 80 port on EC2 with no reason

--- 1. Simple Express Server/index.js
+++ 1. Simple Express Server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+const port = 3000;
+
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
-app.listen(80, () => console.log("Server is running on port 80"));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
